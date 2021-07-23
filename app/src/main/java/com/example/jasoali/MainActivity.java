bb891c42f0631dfc5644f572f9b99ee40ccb25c9
amoohashem <amoo.hashem.mehraban@gmail.com>
2021-07-24 00:23:59 +0430

    do back4app settings

--- app/src/main/java/com/example/jasoali/MainActivity.java
+++ app/src/main/java/com/example/jasoali/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.jasoali;
 
+import com.parse.Parse;
+
 import android.os.Bundle;
+import android.util.Log;
+
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.parse.ParseObject;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
@@ -10,12 +15,32 @@ import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        Parse.initialize(new Parse.Configuration.Builder(this)
+                .applicationId(getString(R.string.back4app_app_id))
+                .clientKey(getString(R.string.back4app_client_key))
+                .server(getString(R.string.back4app_server_url))
+                .build());
+
+        ParseObject firstObject = new ParseObject("FirstClass");
+        firstObject.put("message", "Hey ! First message from android. Parse is now connected");
+        firstObject.saveInBackground(e -> {
+            if (e != null) {
+                Log.e("MainActivity", e.getLocalizedMessage());
+            } else {
+                Log.d("MainActivity", "Object saved.");
+            }
+        });
+
+
         setContentView(R.layout.activity_main);
+
         BottomNavigationView navView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
